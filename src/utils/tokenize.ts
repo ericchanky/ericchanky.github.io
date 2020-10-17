5ddb3a4193e2f02110a5305e8e12f9cdd93b5bb9
Eric Chan <eric.cky715@gmail.com>
2020-10-17 14:13:38 +0800

    update sort algo

--- src/utils/tokenize.ts
+++ src/utils/tokenize.ts
@@ -2,6 +2,18 @@ import { shuffle } from 'lodash'
 
 const salt = 'ybfDNvPQJz5kTncAY2MWFs8ueiSw4BpLX6GqKx3h9trUZgVRdEC1am7H0'
 
+const sort = <T>(arr: T[], cb: (a: T, b: T) => number) => {
+  const nArr = arr.slice()
+  for (let i = 0; i < nArr.length; i++) {
+    for (let j = i+1; j < nArr.length; j++) {
+      if (cb(nArr[i], nArr[j]) > 0) {
+        [nArr[i], nArr[j]] = [nArr[j], nArr[i]]
+      }
+    }
+  }
+  return nArr
+}
+
 export const generateSalt = (s = salt) => {
   return shuffle(s.split('')).join('')
 }
@@ -24,9 +36,9 @@ export const randomSalt = (ctx: string, s = salt) => {
     return acc + cur.charCodeAt(0)
   }, 0)
 
-  const newSalt = s.split('').sort((a, b) => {
-    return a.charCodeAt(0) * b.charCodeAt(0) % n - a.charCodeAt(0)
-  }).join('')
+  const newSalt = sort(s.split(''), (a, b) => {
+    return ((a.charCodeAt(0) * b.charCodeAt(0)) % n) - a.charCodeAt(0)
+  }).join()
 
   return newSalt
 }
